61009dab48e6b949231e818881354433ae25438d
Hermis999 <hermisjulian999@gmail.com>
2025-02-05 16:05:50 -0500

    +

--- frontendapi/src/components/Mangas/Mangasindex.tsx
+++ frontendapi/src/components/Mangas/Mangasindex.tsx
@@ -42,9 +42,9 @@ const handleDelete = async (id) => {
 
     return (
         <div>
-            <div className="mt-2 px-5 ">
+            <div className="mt-2 mx-20 px-5 py-4 flex flex-wrap justify-between">
                 <h2 className="text-2xl font-bold mb-4">Lista de Actividades</h2>
-                <a href="/productos/nuevo" className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded-full">
+                <a href="/productos/nuevo" className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded-full">
                     Crear nuevo Manga
                 </a>               
             </div>
@@ -57,8 +57,8 @@ const handleDelete = async (id) => {
                     className="bg-gray-800 p-4 rounded-2xl shadow-lg relative"
                     >
                     <h3 className="text-xl font-semibold">{manga.title}</h3>
-                    <p className="text-gray-400">{manga.price}</p>
-                    <p className="text-gray-400">{manga.description || "Sin descripción"}</p>
+                    <p className="text-yellow-400">Precio: {manga.price}</p>
+                    <p className="text-gray-400">Descripcion: {manga.description || "Sin descripción"}</p>
                     <div className="flex justify-end space-x-2 mt-4">
                         <button
                         className="flex items-center px-3 py-1 bg-indigo-600 text-white rounded-lg hover:bg-indigo-500"
